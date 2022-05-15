04a25ef5f246f5314e7d6578ae4386c755535a2a
Kazuhiro Haraguchi <arx.solid@gmail.com>
2022-05-15 13:38:53 +0900

    write docstring

--- xdg.v
+++ xdg.v
@@ -2,20 +2,20 @@ module xdg
 
 import os
 
-// config_path return $XDG_CONFIG_HOME/{app}/{file} path
-pub fn config_path(app_name string, config_fname string) string {
+// config_path return string: $XDG_CONFIG_HOME/[app_name]/[file] or $HOME/.config/[app_name]/[file]
+pub fn config_path(app_name string, fname string) string {
 	config_dir := os.config_dir() or { panic(err) }
-	config_path := os.join_path(config_dir, app_name, config_fname)
+	config_path := os.join_path(config_dir, app_name, fname)
 	return config_path
 }
 
-// data_dir return $XDG_DATA_HOME directory path
+// data_dir return string: $XDG_DATA_HOME or $HOME/.local/share directory
 pub fn data_dir() string {
 	return os.getenv_opt('XDG_DATA_HOME') or { os.join_path(os.home_dir(), '.local', 'share') }
 }
 
-// data_path return $XDG_DATA_HOME/{app}/{file} path
-pub fn data_path(app_name string, data_fname string) string {
-	data_path := os.join_path(data_dir(), app_name, data_fname)
+// data_path return string: $XDG_DATA_HOME/{app_name}/{file} or $HOME/.local/share/[app_name]/[file]
+pub fn data_path(app_name string, fname string) string {
+	data_path := os.join_path(data_dir(), app_name, fname)
 	return data_path
 }
